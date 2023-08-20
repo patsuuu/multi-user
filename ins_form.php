000323b5185a07ecfeca051a133c2e62d3147107
AKO TO <134520855+patsuuu@users.noreply.github.com>
2023-08-20 21:22:43 +0800

    Add files via upload

---
+++ ins_form.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Data Insertion Form</title>
+</head>
+<body>
+<a href="user3_page.php?>">Back</a>
+    <center>
+        <h2>DATA INSERT</h2>
+        <form method="post" action="inss_data.php">
+            <label>Title:</label>
+            <input type="text" name="title" required><br><br>
+
+            <label>Details:</label>
+            <input type="text" name="details" required><br><br>
+
+            <label>Authors:</label>
+            <input type="text" name="authors" required><br><br>
+
+            <input type="submit" value="Insert Data">
+        </form>
+    </center>
+</body>
+</html>
---
+++ user3_page.php
@@ -0,0 +1,94 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+if(!isset($_SESSION['user_name'])){
+   header('location:login_form.php');
+}
+
+?>
+<a href="admin3_page.php" class="btn">logout</a>
+   <!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Data List</title>
+</head>
+<body>
+     <!-- Search Data Form -->
+     <center>
+   
+        <h2>USER 3 PAGE</h2>
+        <form method="get" action="sr.php">
+            <label>Search by Title:</label>
+            <input type="text" name="search_title">
+            <input type="submit" value="Search">
+        </form>
+    </center>
+    <center>
+        <p><p>
+        
+        <table border="2">
+            <tr>
+                <th>TITLE</th>
+                <th>PDF</th>
+                <th>UPDATE</th>
+                <th>DELETE</th>
+                
+            </tr>
+
+            <?php
+            include "conn.php";
+
+            // Fetch data from the 'data' table
+            $sql = "SELECT * FROM dataaa";
+            $result = $con->query($sql);
+
+            // Check if the query was successful and there are rows in the result
+            if ($result && $result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    $title = $row["title"];
+                    $details = $row["details"];
+                    $authors = $row["authors"];
+            ?>
+
+
+                    <tr>
+                    <td><?php echo $title; ?></td>
+                    <td><a href="vvvv.php?title=<?php echo urlencode($title); ?>">View</a></td>
+                        <td><a href="delete.php" class="btn">Update</a></td>
+                    
+                        <td><a href="delete.php" class="btn">Delete</a></td>
+                        
+                        
+                    </tr>
+            <?php
+                }
+            } else {
+                echo "<tr><td colspan='2'>0 results!!</td></tr>";
+            }
+            $con->close();
+            ?>
+
+        </table>
+        <center>
+              <a href="ins_form.php?title=<?php echo urlencode($title); ?>">Add data</a>
+        </center>
+       
+    </center>
+<p><p>
+
+
+   
+   
+</body>
+</html>
+
+      
+   </div>
+
+</div>
+
+</body>
+</html>
